6f33d6ac34064902a4e8b3f04a41bd08344f52cc
KJ OBrien <karen@flo-2d.com>
2024-09-05 20:30:25 -0400

    Version 0 Commit

--- app/src/main/java/com/jblearning/tictactoev0/MainActivity.java
+++ app/src/main/java/com/jblearning/tictactoev0/MainActivity.java
@@ -1,24 +1,42 @@
 package com.jblearning.tictactoev0;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.graphics.Point;
+import android.widget.Button;
+import android.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private Button[][] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        // setContentView(R.layout.activity_main);
+        buildGuiByCode();
+    }
+
+    public void buildGuiByCode() {
+        // Get width of the screen
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        int w = size.x / TicTacToe.SIDE;
+
+        // Create the layout manager as a GridLayout
+        GridLayout gridLayout = new GridLayout(this);
+        gridLayout.setColumnCount(TicTacToe.SIDE);
+        gridLayout.setRowCount(TicTacToe.SIDE);
+
+        // Create the buttons and add them to gridLayout
+        buttons = new Button[TicTacToe.SIDE][TicTacToe.SIDE];
+        for (int row = 0; row < TicTacToe.SIDE; row++) {
+            for (int col = 0; col < TicTacToe.SIDE; col++) {
+                buttons[row][col] = new Button(this);
+                gridLayout.addView(buttons[row][col], w, w);
+            }
+        }
+
+        // Set gridLayout as the View of this Activity
+        setContentView(gridLayout);
     }
 }
